625f9af14193f8898c40bb9b142b325bce8bdbd6
Martin Hramiak <orcwarlord@gmail.com>
2022-12-30 14:20:08 +0000

    Changes to book controller

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -7,11 +7,6 @@ use App\Models\Book;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Input;
-
-
-
-
 
 class BookController extends Controller
 {
